e8f23b8ed3592e4af1de8e3935c2b0e639af95ec
lrbell17 <lukbell17@gmail.com>
2025-01-16 16:05:11 -0800

    Section 4: Maps

--- shared/sec02/db/load.go
+++ shared/sec02/db/load.go
@@ -2,12 +2,12 @@ package db
 
 import (
 	"encoding/json"
-  "errors"
-  "fmt"
+	"errors"
+	"fmt"
 	"os"
 
+	"github.com/lrbell17/gotutorial/shared/input"
 	log "github.com/sirupsen/logrus"
-	"github.com/striversity/glft/shared/input"
 )
 
 const (
@@ -26,9 +26,9 @@ var _db database
 
 // Load Person records from people.json file into database, return nil on success or an error value
 func Load() (err error) {
-    // to ensure that we can always load the data, use the user's GOPATH
+	// to ensure that we can always load the data, use the user's GOPATH
 
-  dbFile := fmt.Sprintf(".%c%s", os.PathSeparator, jsonFile) 
+	dbFile := fmt.Sprintf(".%c%s", os.PathSeparator, jsonFile)
 	f, err := os.Open(dbFile)
 	if nil != err {
 		return errors.New("Can't open people.json. Did you copy people.json from shared/sec02/db to current directory?")
