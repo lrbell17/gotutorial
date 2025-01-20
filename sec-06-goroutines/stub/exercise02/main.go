da23a66d4943cfe26703d8338d8dd05453260945
lrbell17 <lukbell17@gmail.com>
2025-01-19 16:20:53 -0800

    Section 6: Goroutines - exercise 2

---
+++ sec-06-goroutines/stub/exercise02/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	capacity = 100000
+	workers  = 6
+)
+
+var (
+	nums []int
+	wg   sync.WaitGroup
+)
+
+func main() {
+
+	wg.Add(workers)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			slice := subSlice(nums[:], workers, i)
+			primeSearch(i+1, slice)
+		}()
+	}
+
+	wg.Wait()
+
+}
+
+func init() {
+	nums = make([]int, capacity)
+	for i := range nums {
+		nums[i] = i + 1
+	}
+}
+
+func primeSearch(workerId int, slice []int) {
+
+	for i := 1; i < len(slice); i++ {
+
+		v := slice[i]
+		prime := true
+
+		for j := 2; j < v/2+1; j++ {
+			if v%j == 0 {
+				prime = false
+				break
+			}
+		}
+		if prime {
+			fmt.Println(v)
+		}
+	}
+	wg.Done()
+}
