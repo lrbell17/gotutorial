fbb835fa8ea7c639e07dd43354857fae835da672
lrbell17 <lukbell17@gmail.com>
2025-02-22 16:34:00 -0800

    Section 10: Standard packages - exercise 4

--- sec-10-standard-packages/stub/exercise04/main.go
+++ sec-10-standard-packages/stub/exercise04/main.go
@@ -20,11 +20,38 @@ func main() {
 
 // intToStr converts an int to a string without any help from any packages
 func intToStr(i int) (s string) {
-	// TODO 1
-	return
+	if i == 0 {
+		return "0"
+	}
+
+	// convert each digit to string
+	for i != 0 {
+		s += string(i%10 + '0')
+		i = i / 10
+	}
+
+	// reverse order
+	r := []rune(s)
+	for x, y := 0, len(s)-1; x < y; x, y = x+1, y-1 {
+		r[x], r[y] = r[y], r[x]
+	}
+
+	return string(r)
 }
+
 // f64ToStr converts a float64 to a string without any help from any packages
 func f64ToStr(f float64) (s string) {
-	// TODO 2
+
+	whole := int(f)
+
+	s = intToStr(whole) + "."
+
+	next := f - float64(whole)
+	for i := 0; i < 3; i++ {
+		dec := int(next * 10)
+		s += intToStr(dec)
+		next = next*10 - float64(dec)
+	}
+
 	return
 }
